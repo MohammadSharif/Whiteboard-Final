aa2cde9e19d6898f34ed0656436ffe64efd5c02e
Mohammad <momosharif58@gmail.com>
2016-05-07 23:23:07 -0700

    Initial commit

---
+++ src/WhiteboardGUI.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+/**
+ * Created by Momo on 5/7/16.
+ */
+public class WhiteboardGUI extends JFrame {
+    public static void main(String[] args) {
+        new WhiteboardGUI();
+    }
+
+    public WhiteboardGUI(){
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
