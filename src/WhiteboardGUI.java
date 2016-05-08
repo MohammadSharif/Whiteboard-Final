3b11bec7b2274cede612f5d64a226b87de94453d
Mohammad <momosharif58@gmail.com>
2016-05-08 00:23:18 -0700

    Created Canvas panel as well as Controls panel, need to add buttons to controls next

--- src/WhiteboardGUI.java
+++ src/WhiteboardGUI.java
@@ -10,11 +10,30 @@ public class WhiteboardGUI extends JFrame {
     }
 
     public WhiteboardGUI(){
+        Canvas canvas = new Canvas();
+        Controls controls = new Controls();
         this.setPreferredSize(new Dimension(800, 400));
         this.setSize(this.getPreferredSize());
         this.setLocationRelativeTo(null);
-
+        this.setLayout(new BorderLayout());
+        this.add(canvas, BorderLayout.CENTER);
+        this.add(controls, BorderLayout.WEST);
         this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    private class Canvas extends JPanel{
+        public Canvas(){
+            this.setPreferredSize(new Dimension(400, 400));
+            this.setSize(this.getPreferredSize());
+        }
+    }
+
+    private class Controls extends JPanel{
+        public Controls(){
+            this.setPreferredSize(new Dimension(400, 400));
+            this.setSize(this.getPreferredSize());
+            this.setBackground(Color.black);
+        }
     }
 }
