b639fc51a22a42ea061d4a4f82c8a36e10b0fd50
Mohammad <momosharif58@gmail.com>
2016-05-08 00:59:46 -0700

    Began adding controls, all buttons there need to implement the table at the bottom

--- src/WhiteboardGUI.java
+++ src/WhiteboardGUI.java
@@ -1,5 +1,8 @@
+import javax.imageio.plugins.jpeg.JPEGHuffmanTable;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Momo on 5/7/16.
@@ -31,9 +34,75 @@ public class WhiteboardGUI extends JFrame {
 
     private class Controls extends JPanel{
         public Controls(){
+            ShapeButtons shapeButtons = new ShapeButtons();
+            TextStyleButtons textStyleButtons = new TextStyleButtons();
+            FrontOrBackButtons frontOrBackButtons = new FrontOrBackButtons();
+            frontOrBackButtons.setAlignmentX(CENTER_ALIGNMENT);
+            textStyleButtons.setAlignmentX(CENTER_ALIGNMENT);
+            shapeButtons.setAlignmentX(CENTER_ALIGNMENT);
+            this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+            JButton setColor = new JButton("Set Color");
+            setColor.setAlignmentX(CENTER_ALIGNMENT);
+            setColor.addActionListener(new ListenForButton());
+
             this.setPreferredSize(new Dimension(400, 400));
             this.setSize(this.getPreferredSize());
             this.setBackground(Color.black);
+            this.add(shapeButtons);
+            this.add(setColor);
+            this.add(textStyleButtons);
+            this.add(frontOrBackButtons);
+
+
+        }
+    }
+
+    private class ShapeButtons extends JPanel{
+        public ShapeButtons(){
+            ListenForButton listenForButton = new ListenForButton();
+            JButton[] buttons = {new JButton("Rect"), new JButton("Oval"), new JButton("Line"), new JButton("Text")};
+            this.setLayout(new FlowLayout());
+            this.add(new JLabel("Add"));
+            for(JButton button: buttons){
+                this.add(button);
+                button.addActionListener(listenForButton);
+            }
+            this.setPreferredSize(new Dimension(400, 10));
+            this.setSize(this.getPreferredSize());
+        }
+    }
+
+    private class TextStyleButtons extends JPanel{
+        public TextStyleButtons(){
+            this.setLayout(new FlowLayout());
+            this.add(new TextField("Whiteboard!", 15));
+            this.add(new JButton("Temp Placeholder"));
+        }
+    }
+
+    private class FrontOrBackButtons extends JPanel{
+        public FrontOrBackButtons(){
+            JButton toFront = new JButton("Move To Front");
+            JButton toBack = new JButton("Move To Back");
+            JButton remove = new JButton("Remove Shape");
+            toFront.addActionListener(new ListenForButton());
+            toBack.addActionListener(new ListenForButton());
+            remove.addActionListener(new ListenForButton());
+            this.setLayout(new FlowLayout());
+            this.add(toFront);
+            this.add(toBack);
+            this.add(remove);
+
+        }
+    }
+
+
+
+    private class ListenForButton implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
         }
     }
 }
