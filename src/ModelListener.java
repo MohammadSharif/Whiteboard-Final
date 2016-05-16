826c4376802b345d6e3fd48dd9a6971ee701f80c
Mohammad <momosharif58@gmail.com>
2016-05-16 14:47:27 -0700

    Added the ModelListener interface

---
+++ src/ModelListener.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Momo on 5/16/16.
+ * This interface is the model listener, all objects that need to be listened to will implement this interface.
+ */
+public interface ModelListener {
+    public void modelChanged(DShapeModel model);
+}
